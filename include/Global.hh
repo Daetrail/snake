2b653b57d23effb9f15458e057f2b1972ce9ce91
danangthan <cruzmattdustin@gmail.com>
2021-11-10 19:00:03 +0000

    Dimension variables added

---
+++ include/Global.hh
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "SFML/Graphics.hpp"
+
+namespace G
+{
+    const unsigned int WINDOW_WIDTH = 600;
+    const unsigned int WINDOW_HEIGHT = 600;
+    const unsigned int SQUARE_SPACING = 30;
+}
